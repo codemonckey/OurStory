5df3843bd348235c967ba4c8eb746988a85f899d
Josh Gorodinsky <josh.gorodinsky@Joshs-MacBook-Pro.local>
2019-12-03 00:37:29 -0500

    added some html pages

---
+++ HTML_CLIENT/public/main.js
@@ -0,0 +1,12 @@
+const el = document.getElementById("buttonClick");
+
+el.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/loading.html"
+});
+
+
+const te = document.getElementById("test");
+
+te.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/contribute.html"
+});
