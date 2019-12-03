744eeb2462c4bd1397621ebc40fa2b1535db2a72
Isaac Dessert <desserti@mail.gvsu.edu>
2019-12-03 00:46:11 -0500

    fixing git and adding ourstory

--- HTML_CLIENT/OurStoryHTTPServer.java
+++ HTML_CLIENT/OurStoryHTTPServer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,10 +8,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.FileWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
 
 public class OurStoryHTTPServer implements Runnable {
 
@@ -18,11 +21,14 @@ public class OurStoryHTTPServer implements Runnable {
   static final String DEFAULT_FILE = "index.html";
   static final String FILE_NOT_FOUND = "404.html";
   static final String METHOD_NOT_SUPPORTED = "not_supported.html";
+  
   // port to listen connection 
   static final int PORT = 8080;
 
   // Client Connection via Socket Class
   private Socket connect;
+  public int storyTally = 10;
+  public String currentFile;
 
   public OurStoryHTTPServer(Socket c) {
     connect = c;
@@ -78,19 +84,20 @@ public class OurStoryHTTPServer implements Runnable {
 				// we return the not supported file to the client
 				File file = new File(WEB_ROOT, METHOD_NOT_SUPPORTED);
 				int fileLength = (int) file.length();
-				String contentMimeType = "text/html";
+
 				//read content to return to client
 				byte[] fileData = readFileData(file, fileLength);
 					
-				// we send HTTP Headers with data to client
+				//here is the ip header
 				out.println("HTTP/1.1 501 Not Implemented");
 				out.println("Server: Java HTTP Server from SSaurel : 1.0");
 				out.println("Date: " + new Date());
-				out.println("Content-type: " + contentMimeType);
+				out.println("Content-type: " + "text/html");
 				out.println("Content-length: " + fileLength);
-				out.println(); // blank line between headers and content, very important !
-				out.flush(); // flush character output stream buffer
-				// file
+				out.println();
+				out.flush(); 
+
+
 				dataOut.write(fileData, 0, fileLength);
 				dataOut.flush();
 				
@@ -109,7 +116,7 @@ public class OurStoryHTTPServer implements Runnable {
 					
 					// send HTTP Headers
 					out.println("HTTP/1.1 200 OK");
-					out.println("Server: Java HTTP Server from SSaurel : 1.0");
+					out.println("Server: Java HTTP Server");
 					out.println("Date: " + new Date());
 					out.println("Content-type: " + content);
 					out.println("Content-length: " + fileLength);
@@ -124,7 +131,7 @@ public class OurStoryHTTPServer implements Runnable {
 			
 		} catch (FileNotFoundException fnfe) {
 			try {
-				fileNotFound(out, dataOut, fileRequested);
+				fourOhFour(out, dataOut, fileRequested);
 			} catch (IOException ioe) {
 				System.err.println("Error with file not found exception : " + ioe.getMessage());
 			}
@@ -160,31 +167,60 @@ public class OurStoryHTTPServer implements Runnable {
 		return fileData;
 	}
 	
+	//this method is for iterating the tally
+	private void iterateTally(){
+		if(storyTally == 0) {storyTally = 10;} else {storyTally-=1;}
+	}
+
 	// return supported MIME Types
 	private String getContentType(String fileRequested) {
 		if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
 			return "text/html";
 		else if (fileRequested.endsWith(".css"))
-		return "text/css";
-			else
+			return "text/css";
+			else if (fileRequested.endsWith(".js"))
+			return "text/js";
+		else
 			return "text/plain";
 	}
 	
-	private void fileNotFound(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException {
+	private void fourOhFour(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException {
 		File file = new File(WEB_ROOT, FILE_NOT_FOUND);
 		int fileLength = (int) file.length();
-		String content = "text/html";
 		byte[] fileData = readFileData(file, fileLength);
 		
 		out.println("HTTP/1.1 404 File Not Found");
 		out.println("Server: Java HTTP Server from SSaurel : 1.0");
 		out.println("Date: " + new Date());
-		out.println("Content-type: " + content);
+		out.println("Content-type: " + "text/html");
 		out.println("Content-length: " + fileLength);
 		out.println(); // blank line between headers and content, very important !
 		out.flush(); // flush character output stream buffer
 		
 		dataOut.write(fileData, 0, fileLength);
 		dataOut.flush();
+	}
+
+	private void createHTML(String updated) {
+		try{
+		BufferedWriter bw = new BufferedWriter(new FileWriter(currentFile));
+		if(storyTally == 10){
+			bw.write("<html><head><title>" + updated + "</title></head><body><p>");
+		}
+		else if(storyTally == 0){
+			bw.write(updated + "</p></body></html>");
+		}
+		else
+			bw.write(updated);
+		bw.close();
+		}
+		catch(IOException e){
+		System.out.println(e);
+		}
+	}
+
+	private void randomizeFileName(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("File-ddMMyy-hhmmss.SSS.txt");
+		currentFile = new File(simpleDateFormat.format( new Date() )).getName();
 	}
 }
