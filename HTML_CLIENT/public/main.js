d03ae3064c2601974b3f0bf5cedb3cdefd04d9f3
dejongry <dejongr@mail.gvsu.edu>
2019-12-04 02:05:47 -0500

    Changed some HTML

--- HTML_CLIENT/public/main.js
+++ HTML_CLIENT/public/main.js
@@ -9,4 +9,26 @@ const te = document.getElementById("test");
 
 te.addEventListener("click", function() {
     location.href = "http://localhost:8080/public/contribute.html"
+<<<<<<< Updated upstream
+=======
+});
+
+const te2 = document.getElementById("test2");
+
+te2.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/titleSentence.html"
+});
+
+
+const hs = document.getElementById("goHome");
+
+hs.addEventListener("click", function() {
+    location.href = "http://localhost:8080"
+});
+
+
+const osc = document.getElementById("oldStoriesClicked");
+osc.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/oldStories.html"
+>>>>>>> Stashed changes
 });
