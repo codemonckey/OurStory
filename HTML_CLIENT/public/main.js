28cbd5fe25fe7b49991dfd779cbe7422817a9cd7
dejongry <dejongr@mail.gvsu.edu>
2019-12-04 02:31:54 -0500

    Changed some HTML

--- HTML_CLIENT/public/main.js
+++ HTML_CLIENT/public/main.js
@@ -11,6 +11,11 @@ te.addEventListener("click", function() {
     location.href = "http://localhost:8080/public/contribute.html"
 });
 
+const te2 = document.getElementById("test2");
+
+te2.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/titleSentence.html"
+});
 
 const hs = document.getElementById("goHome");
 
