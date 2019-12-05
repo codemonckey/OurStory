e209660c2ebda7878c6a9d2120f70a3600b239eb
dejongry <dejongr@mail.gvsu.edu>
2019-12-04 21:54:31 -0500

    Got all the buttons working besides home

--- HTML_CLIENT/public/main.js
+++ HTML_CLIENT/public/main.js
@@ -4,6 +4,11 @@ el.addEventListener("click", function() {
     location.href = "http://localhost:8080/public/loading.html"
 });
 
+const osc = document.getElementById("oldStoriesClicked");
+
+osc.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/oldStories.html"
+});
 
 const te = document.getElementById("test");
 
@@ -17,14 +22,8 @@ te2.addEventListener("click", function() {
     location.href = "http://localhost:8080/public/titleSentence.html"
 });
 
-const hs = document.getElementById("goHome");
+const gh = document.getElementById("goHome");
 
-hs.addEventListener("click", function() {
-    location.href = "http://localhost:8080/public/index.html"
-});
-
-
-const osc = document.getElementById("oldStoriesClicked");
-osc.addEventListener("click", function() {
-    location.href = "http://localhost:8080/public/oldStories.html"
+gh.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/home.html"
 });
