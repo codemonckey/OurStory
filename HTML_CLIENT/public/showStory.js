d293e5ebdc95f68eb6cf91a2716b941409e92dce
dejongry <dejongr@mail.gvsu.edu>
2019-12-05 12:01:03 -0500

    Changes

---
+++ HTML_CLIENT/public/showStory.js
@@ -0,0 +1,23 @@
+const ft = document.getElementById("1Title");
+
+ft.addEventListener("click", function() {
+    location.href = "http://localhost:8080/public/loading.html"
+});
+
+const st = document.getElementById("2Title");
+
+const tt = document.getElementById("3Title");
+
+const fot = document.getElementById("4Title");
+
+const fit = document.getElementById("5Title");
+
+const sit = document.getElementById("6Title");
+
+const set = document.getElementById("7Title");
+
+const et = document.getElementById("8Title");
+
+const nt = document.getElementById("9Title");
+
+const tent = document.getElementById("10Title");
